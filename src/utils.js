145241d3b1a1cd9575abb799a69c7511842e183a
doananh234 <doananh234@gmail.com>
2022-07-14 22:24:58 +0700

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,5 @@
 const elHasClass = (el, toFind) => {
-  let cls = el.className;
+  let cls = el.classNames;
   cls = cls && cls.toString();
   if (cls && cls.split(' ').indexOf(toFind) >= 0) return 1;
 }
